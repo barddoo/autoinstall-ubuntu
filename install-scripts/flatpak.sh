a90d9ef5ae56b8651fba7fba17fcc38c5b1cc27a
Charles Fonseca <charles@gardyn.tech>
2024-11-16 18:14:24 -0300

    updates on nvim

---
+++ install-scripts/flatpak.sh
@@ -0,0 +1,10 @@
+flatpak install flathub com.getpostman.Postman \
+  com.discordapp.Discord \
+  com.spotify.Client \
+  com.slack.Slack \
+  com.protonvpn.www \
+  com.valvesoftware.Steam \
+  io.github.zen_browser.zen \
+  io.missioncenter.MissionCenter \
+  me.iepure.devtoolbox \
+  me.proton.Pass \
--- install-scripts/nvim.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install neovim
-sudo apt-get install 
-python-dev python-pip python3-dev python3-pip
