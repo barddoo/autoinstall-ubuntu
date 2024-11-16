834354808e9a0c3233c227779e4baf698ffa91bd
Charles Fonseca <charles@gardyn.tech>
2024-11-16 17:54:31 -0300

    updates

---
+++ install-scripts/lazyvim.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+mv ./config/lazyvim/* ~/.config/nvim/lua
---
+++ install-scripts/nvim.sh
@@ -0,0 +1,3 @@
+sudo apt-get install neovim
+sudo apt-get install 
+python-dev python-pip python3-dev python3-pip
