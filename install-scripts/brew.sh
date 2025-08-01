aafe216cd4a0e6d8487ae9292940fa4752769879
Charles Fonseca <pseudo.charles@proton.me>
2025-08-01 16:52:11 -0300

    Update brew.sh

--- install-scripts/brew.sh
+++ install-scripts/brew.sh
@@ -1,3 +1,5 @@
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew tap oven-sh/bun
+
 brew install bat bun direnv fd fzf lazygit node@22 poetry pre-commit python@3.11 ripgrep virtualenv zig ghostty zsh zoxide azure-cli podman
